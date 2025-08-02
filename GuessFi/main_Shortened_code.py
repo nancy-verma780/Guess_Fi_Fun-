627b3b3f1f6a13f3ed496e5a99f5f9821b84708a
nancy-verma780 <nancy45815@gmail.com>
2025-08-02 22:57:19 +0530

    GUESSIFY GAME

--- GuessFi/main_Shortened_code.py
+++ GuessFi/main_Shortened_code.py
@@ -1,53 +1,13 @@
 import random
-print("Welcome to Snake, Water, Gun game!")
-computer = random.choice([1, -1, 0])
-youstr = input("Enter your choice: ")
-youDict = {"s": 1, "w": -1, "g": 0}
-reverseDict = {1: "snake", -1: "water", 0: "gun"}
-
-you = youDict[youstr]
-
-# Buy now we have 2 number (variables), you and computer
-
-print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
-
-if(computer == you):
-    print("Draw!")
-
-else:
-    '''
-    #if(computer == -1 and you == 1):
-     #print("You win!")
-     #print("Computer chose water")
-
-    #elif(computer == -1 and you == 0):
-      #print("You lose!")
-      #print("Computer chose water")
-
-    #elif(computer == 1 and you == 0):
-      #print("You win!")
-      #print("Computer chose snake")
-
-    #elif(computer == 1 and you == -1):
-      #print("You lose!")
-      #print("Computer chose snake")
-
-    #elif(computer == 0 and you == 1):
-      #print("You lose!")
-      #print("Computer chose Gun")
-
-    #elif(computer == 0 and you == -1):
-      #print("You win!")
-      #print("Computer chose Gun")
-
-    #else:
-      #print("Something went wrong")
-
-      This below logic is written on the basis of the valueof computer - You !
-      '''
-    
-    if((computer - you) == -1 or (computer - you) == 2):
-        print("You win!")
-    else:
-        print("You lose!")
-
+n = random.randint(1, 100)
+a = -1
+guesses = 1
+while(a != n):
+    a = int(input("Guess the Number: "))
+    if(a > n):
+        print("Lower Number please!")
+        guesses +=1
+    elif(a<n):
+        print("Higher Number please!")
+        guesses +=1
+print(f"You have guesses the number {n} correctly in {guesses} attempt")
